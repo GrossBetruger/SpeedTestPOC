64efa508fa3864307662d352350b725ead955e33
Oren Koriat <oren@cynerio.co>
2018-07-07 16:37:39 +0300

    added ratio and hours args added inverse ratio

--- python/susp_images.sql
+++ python/susp_images.sql
@@ -4,7 +4,7 @@ select start_measuring_timestamp, file_download_ratekbper_sec, download_rate_in_
 from comparison_info join file_download_info on comparison_info.file_download_info_id = file_download_info.id
 join speed_test_web_site on speed_test_web_site.id = comparison_info.speed_test_web_site_download_info_id where
 
-((file_download_ratekbper_sec / ((download_rate_in_mbps / 8.0) * 1024))  > 10
+((file_download_ratekbper_sec / ((download_rate_in_mbps / 8.0) * 1024))  > {}
 or (file_download_ratekbper_sec / ((download_rate_in_mbps / 8.0) * 1024))  < {})
 and (current_timestamp - interval '{} hour')::abstime::int::bigint < start_measuring_timestamp / 1000
 
