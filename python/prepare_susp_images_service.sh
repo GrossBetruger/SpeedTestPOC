2ed1c47db5042d32ac1bda65eb80f675edd234ff
Oren Koriat <oren@cynerio.co>
2018-07-07 18:16:12 +0300

    typo fix

--- python/prepare_susp_images_service.sh
+++ python/prepare_susp_images_service.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-zip sups_img_service.zip susp_images.py susp_images.sql image_service_requirements.txt 
+zip susp_img_service.zip susp_images.py susp_images.sql image_service_requirements.txt 
